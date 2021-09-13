b563351905f40fb2910ffce9e7f5e4e18b91b402
Ivan Zukkel <85585191+IvanZu2kel@users.noreply.github.com>
2021-09-13 19:14:56 +0300

    Delete V1_1__blog_engine_db.sql

--- BlogEngine/src/main/resources/db/migration/V1_1__blog_engine_db.sql
+++
@@ -1,9 +0,0 @@
-alter table post_comments add constraint post_comments_parent_id_fk foreign key (parent_id) references post_comments (id);
-alter table post_comments add constraint post_comments_post_id_fk foreign key (post_id) references posts (id);
-alter table post_comments add constraint post_comments_user_id_fk foreign key (user_id) references users (id);
-alter table post_votes add constraint post_votes_user_id_fk foreign key (user_id) references users (id);
-alter table post_votes add constraint post_votes_post_id_fk foreign key (post_id) references posts (id);
-alter table posts add constraint posts_moderator_id_fk foreign key (moderator_id) references users (id);
-alter table posts add constraint posts_user_id_fk foreign key (user_id) references users (id);
-alter table tag2post add constraint tag2post_post_id_fk foreign key (post_id) references posts (id);
-alter table tag2post add constraint tag2post_tag_id_fk foreign key (tag_id) references tags (id);
