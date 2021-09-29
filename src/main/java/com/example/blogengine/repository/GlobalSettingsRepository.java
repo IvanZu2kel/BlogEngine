434f63ff743e9ee7a4697be00a5f1b721a19520e
Ivan Zu2kel <ivan_zukkel@mail.ru>
2021-09-29 18:40:33 +0300

    Part 2. Corrected. 29.09

---
+++ src/main/java/com/example/blogengine/repository/GlobalSettingsRepository.java
@@ -0,0 +1,12 @@
+package com.example.blogengine.repository;
+
+import com.example.blogengine.model.GlobalSettings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface GlobalSettingsRepository extends JpaRepository<GlobalSettings, Integer> {
+
+    @Query(nativeQuery = true, value = "select s.* from global_settings s where s.code = :code")
+    GlobalSettings findAllGlobalSettings(@Param("code") String code);
+}
--- src/main/java/com/example/blogengine/service/implementation/SettingsServiceImpl.java
+++ src/main/java/com/example/blogengine/service/implementation/SettingsServiceImpl.java
@@ -1,17 +1,27 @@
 package com.example.blogengine.service.implementation;
 
 import com.example.blogengine.api.response.SettingsResponse;
+import com.example.blogengine.repository.GlobalSettingsRepository;
 import com.example.blogengine.service.SettingsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SettingsServiceImpl implements SettingsService {
 
+    private final GlobalSettingsRepository globalSettingsRepository;
+
+    @Autowired
+    public SettingsServiceImpl(GlobalSettingsRepository globalSettingsRepository) {
+        this.globalSettingsRepository = globalSettingsRepository;
+    }
+
     @Override
     public SettingsResponse getGlobalSettings(){
         SettingsResponse settingsResponse = new SettingsResponse();
-        settingsResponse.setPostPremoderation(true);
-        settingsResponse.setStatisticIsPublic(true);
+        settingsResponse.setMultiuserMode(globalSettingsRepository.findAllGlobalSettings("MULTIUSER_MODE").getValue().equals("YES"));
+        settingsResponse.setPostPremoderation(globalSettingsRepository.findAllGlobalSettings("POST_PREMODERATION").getValue().equals("YES"));
+        settingsResponse.setStatisticIsPublic(globalSettingsRepository.findAllGlobalSettings("STATISTICS_IS_PUBLIC").getValue().equals("YES"));
 
         return settingsResponse;
     }
