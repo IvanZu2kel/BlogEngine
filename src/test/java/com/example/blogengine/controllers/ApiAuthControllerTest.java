3a6653a28459ddd9fa30c18c7f0c28b758ff6e6b
Ivan Zu2kel <ivan_zukkel@mail.ru>
2022-01-05 14:28:22 +0300

    Правка тестов Добавление данных для heroku

--- src/test/java/com/example/blogengine/controllers/ApiAuthControllerTest.java
+++ src/test/java/com/example/blogengine/controllers/ApiAuthControllerTest.java
@@ -190,10 +190,10 @@ public class ApiAuthControllerTest extends AbstractTest {
 
     @Test
     void login() throws Exception {
-        LoginRequest loginRequest = new LoginRequest()
-                .setEmail("test@test.ru")
-                .setPassword("test@test.ru");
-        User user = userRepository.findByEmail("test@test.ru").orElseThrow();
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("test2@test.ru");
+        loginRequest.setPassword("test2@test.ru");
+        User user = userRepository.findByEmail("test2@test.ru").orElseThrow();
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
