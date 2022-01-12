1afcf39e962ccbc51d272d53ac9d90ff9e9f1097
King <ivan_zukkel@mail.ru>
2022-01-12 13:30:12 +0300

    Отправка оформленного email

--- src/main/java/com/example/blogengine/config/MailjetSender.java
+++ src/main/java/com/example/blogengine/config/MailjetSender.java
@@ -9,20 +9,40 @@ import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.resource.Emailv31;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 @Service
-@AllArgsConstructor
 public class MailjetSender {
     private final MailjetRegistrationParam mailjet;
+    private final String recovery;
+
+    private String htmlTextRecovery;
+
+    public MailjetSender(MailjetRegistrationParam mailjet,
+                         @Value(value = "${html.file.recovery}")String recovery) {
+        this.mailjet = mailjet;
+        this.recovery = recovery;
+    }
 
     public void send(String emailTo, String message) throws MailjetException {
         MailjetClient client;
         MailjetRequest request;
         MailjetResponse response;
+        String messageResponse;
+        String[] split = message.split("/");
+        if (Arrays.asList(split).contains("login")) {
+            messageResponse = htmlTextRecovery.replace("$message", message);
+        } else messageResponse = message;
         client = new MailjetClient(ClientOptions.builder().apiKey(mailjet.getKey()).apiSecretKey(mailjet.getSecret()).build());
         request = new MailjetRequest(Emailv31.resource)
                 .property(Emailv31.MESSAGES, new JSONArray()
@@ -36,11 +56,21 @@ public class MailjetSender {
                                                 .put("Name", "DevPub")))
                                 .put(Emailv31.Message.SUBJECT, "Ссылка на восстановалние пароля")
                                 .put(Emailv31.Message.TEXTPART, "")
-                                .put(Emailv31.Message.HTMLPART, message)
+                                .put(Emailv31.Message.HTMLPART, messageResponse)
                                 .put(Emailv31.Message.CUSTOMID, "AppGettingStartedTest")));
         response = client.post(request);
         log.info(String.valueOf(response.getStatus()));
         log.info(String.valueOf(response.getData()));
+    }
+
+    @PostConstruct
+    private void getFileByPathRegister() {
+        File htmlMessageFileRecovery = new File(recovery);
+        try {
+            htmlTextRecovery = FileUtils.readFileToString(htmlMessageFileRecovery);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
